feeeb4c74fcd90d4ccdaac3a4e5e2cd84753da43
vlad0-ss <vladru110mail@gmail.com>
2026-04-05 14:15:21 +0500

    Update teleport script URL for infinite yield

--- main.lua
+++ main.lua
@@ -6480,7 +6480,7 @@ local TeleportCheck = false
 Players.LocalPlayer.OnTeleport:Connect(function(State)
 	if KeepInfYield and (not TeleportCheck) and queueteleport then
 		TeleportCheck = true
-		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()")
+		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/vlad0-ss/infyieldrjmodded/refs/heads/main/main.lua'))()")
 	end
 end)
 
